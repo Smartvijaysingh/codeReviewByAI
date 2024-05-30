df3307ed8ff65bd2b38b5e81ee0da96514fc4c7c
vijays <Vijay.S@sapiens.com>
2024-05-30 10:04:26 +0530

    change

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -4,6 +4,14 @@ public class Test {
 
     public static void main(String args[])
     {
-        System.out.println("Hello World");
+       for(int i=1;i<=5;i++)
+       {
+           for(int j=1;j<=i;j++)
+           {
+               System.out.print(j);
+           }
+           System.out.println(i);
+       }
+
     }
 }
