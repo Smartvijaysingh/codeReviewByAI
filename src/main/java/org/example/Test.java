4e23212400819327d737c6f64a2dabe5d8b69c75
vijays <Vijay.S@sapiens.com>
2024-05-30 09:50:13 +0530

    my new changes

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Test {
+
+    public static void main(String args[])
+    {
+
+        int a=2147483647+1;
+        short b= (short)a;
+        System.out.println(a);
+    }
+}
