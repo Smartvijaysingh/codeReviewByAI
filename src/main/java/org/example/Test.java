fd9ce729fc04b93e0014478573f42f3cf448e69c
vijays <Vijay.S@sapiens.com>
2024-05-30 09:55:16 +0530

    my new changes

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -4,9 +4,6 @@ public class Test {
 
     public static void main(String args[])
     {
-
-        int a=2147483647+1;
-        short b= (short)a;
-        System.out.println(a);
+        System.out.println("Hello World");
     }
 }
